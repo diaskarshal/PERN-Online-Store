d1cf3e089ebe2cfb960610424d37d2fdb5576c91
789shinigami <dias.karshal@gmail.com>
2025-03-10 08:46:20 +0500

    crud

---
+++ server/index.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path")
+const fileUpload = require("express-fileupload");
+const sequelize = require("./db");
+const modules = require("./models/models");
+const router = require("./routes/index");
+const errorHandler = require("./middleware/ErrorHandlingMiddleware");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
+app.use("/api", router);
+
+app.use(errorHandler);
+
+app.get("/", (req, res, next) => {
+  res.status(200).json({ message: "good" });
+});
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`server on ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
